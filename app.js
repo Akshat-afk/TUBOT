a6d0b7482bda2011d0590ba487297ab2db8a3638
Akshat-afk <akshatbhadani06@gmail.com>
2024-03-16 01:16:45 +0530

    initialized nodejs

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+const fs = require("fs");
+
+const port = 3000;
+
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const genAI = new GoogleGenerativeAI("AIzaSyAlWi2dnBTprQP39_pqCiROeXj9ntMpFug");
+
+async function run() {
+  const model = genAI.getGenerativeModel({ model: "gemini-pro" });
+
+  const prompt = "Write a story about a magic backpack.";
+
+  const result = await model.generateContent(prompt);
+  const response = await result.response;
+  const text = response.text();
+  console.log(text);
+}
+
+app.use(express.static('public'))
+app.use('/css', express.static(__dirname+"public/css"))
+app.use("/js", express.static(__dirname + "public/js"))
+app.use("/images", express.static(__dirname + "public/images"))
+
+app.set('views', './views')
+app.set("view engine", "ejs");
+
+
+app.get('', (req,res) => {
+    res.render('index')
+    run();
+})
+
+app.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
+
+
+
+
